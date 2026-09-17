033a4235219a41167277aa8c69bf679accd55142
muhsinashoma <muhsina.akter2@gmail.com>
2026-09-17 17:38:48 +0600

    To Being Updated TT Data Table Instantly and Hide Closed TT from Data Table

--- frontend/components/ui/tt-modal-store.ts
+++ frontend/components/ui/tt-modal-store.ts
@@ -1,37 +1,3 @@
-// //components/ui/tt-modal-store.ts
-
-// "use client"
-
-// import { create } from "zustand"
-// //import { Section } from "@/types/tt"
-// import type {
-//     Section,
-// } from "@/types/tt";
-
-// type TTModalStore = {
-//     open: boolean
-//     data: Section | null
-//     openModal: (data: Section) => void
-//     closeModal: () => void
-// }
-
-// export const useTTModal = create<TTModalStore>((set) => ({
-//     open: false,
-//     data: null,
-
-//     openModal: (data) =>
-//         set({
-//             open: true,
-//             data,
-//         }),
-
-//     closeModal: () =>
-//         set({
-//             open: false,
-//             data: null,
-//         }),
-// }))
-
 
 //components/ui/tt-modal-store.ts
 
